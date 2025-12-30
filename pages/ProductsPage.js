b1c29b60b1facd21c9f41d043eb1ef731fac8e4c
yusrilazharp <yusrilazharpermana@gmail.com>
2025-12-30 10:18:23 +0700

    Add Products page object with add to cart functionality

---
+++ pages/ProductsPage.js
@@ -0,0 +1,15 @@
+export class ProductsPage {
+  constructor(page) {
+    this.page = page;
+    this.addToCartButton = page.getByText('Add to cart').first();
+    this.cartIcon = page.locator('.shopping_cart_link');
+  }
+
+  async addFirstProductToCart() {
+    await this.addToCartButton.click();
+  }
+
+  async goToCart() {
+    await this.cartIcon.click();
+  }
+}
