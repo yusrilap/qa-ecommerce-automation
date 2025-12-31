00edcb2120ead233e032f6b366cda35a538cda2e
yusrilazharp <yusrilazharpermana@gmail.com>
2025-12-31 14:47:26 +0700

    Add smoke and e2e tags to critical checkout flow test

--- tests/e2e/ecommerce-checkout.spec.js
+++ tests/e2e/ecommerce-checkout.spec.js
@@ -5,12 +5,12 @@ import { CartPage } from '../../pages/CartPage';
 import { CheckoutPage } from '../../pages/CheckoutPage';
 import { users } from '../../test-data/users.js';
 
-
-test('@e2e User can login and complete checkout', async ({ page }) => {
+// Smoke test: critical ecommerce checkout flow
+test('@smoke @e2e User can login and complete checkout', async ({ page }) => {
   const loginPage = new LoginPage(page);
   const productsPage = new ProductsPage(page);
   const cartPage = new CartPage(page);
-  const checkoutPage = new CheckoutPage(page);
+  const  checkoutPage = new CheckoutPage(page);
 
   await loginPage.visit();
   await loginPage.login(users.standard.username, users.standard.password);
