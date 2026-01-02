532a9caa329c11e9852225dc35032974f2c23701
yusrilazharp <yusrilazharpermana@gmail.com>
2026-01-02 07:03:30 +0700

    Add multiple user types for ecommerce testing

--- test-data/users.js
+++ test-data/users.js
@@ -2,5 +2,9 @@ export const users = {
   standard: {
     username: 'standard_user',
     password: 'secret_sauce'
+  },
+  locked: {
+    username: 'locked_out_user',
+    password: 'secret_sauce'
   }
 };
